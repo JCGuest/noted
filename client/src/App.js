31318f37b6af3703bac8d57d25a0b74f48b01dc5
johng <jcguest90@gmail.com>
2021-02-18 02:19:00 -0600

    import icon, menu, typeography and draer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import Drawer from '@material-ui/core/Drawer';
 import './App.css';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  // const handleDrawerClose = () => {
+  //   setOpen(false);
+  // };
+
   return (
-    <div className='App'>
-      <Drawer />
+    <div>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Typography variant='h6' noWrap>
+            Persistent drawer
+          </Typography>
+          <IconButton
+            color='pink'
+            aria-label='open drawer'
+            edge='end'
+            onClick={handleDrawerOpen}
+            className={open}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant='persistent' anchor='left' open={open}>
+        <h1>Hello</h1>
+      </Drawer>
     </div>
   );
 }
