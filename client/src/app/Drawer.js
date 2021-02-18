b8371ff940bacc4365a87583e8dbb77a89738d9c
johng <jcguest90@gmail.com>
2021-02-18 10:12:27 -0600

    moved MUIDrawer to Drawer.js

---
+++ client/src/app/Drawer.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Drawer as MUIDrawer } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+function Drawer() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const drawerWidth = 300;
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex'
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      })
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      }),
+      marginRight: drawerWidth
+    },
+    title: {
+      flexGrow: 1
+    },
+    hide: {
+      display: 'none'
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-start'
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      }),
+      marginRight: -drawerWidth
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      }),
+      marginRight: 0
+    }
+  }));
+
+  return (
+    <div>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Typography variant='h6' noWrap>
+            Persistent drawer
+          </Typography>
+          <IconButton
+            className='icon-button'
+            color='pink'
+            aria-label='open drawer'
+            // edge='end'
+            onClick={handleDrawerOpen}
+          >
+            <MenuIcon className='menu-icon' />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <MUIDrawer
+        variant='persistent'
+        anchor='left'
+        open={open}
+        className='drawer'
+      >
+        <h1>Hello</h1>
+      </MUIDrawer>
+    </div>
+  );
+}
+
+export default Drawer;
