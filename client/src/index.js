d754207a029e930947682cf5a418df78ab5edb68
johng <jcguest90@gmail.com>
2021-03-16 13:24:21 -0500

    landing css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './components/App';
 import reducer, { initialState } from './reducer';
 import { StateProvider } from './StateProvider.js';
